640c6eac0a6c7b07319437dd7215c2d9b5073192
FloMoro <flora.moreau@mmibordeaux.com>
2020-11-09 18:07:01 +0100

    recup

---
+++ src/components/Player/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from "./Player.module.css";
+
+const Player = () =>{
+
+    return (
+        <div className = {style.musicplayer}>
+         <p>
+             bonjour
+         </p>
+        </div>
+        
+      );
+
+}
+
+export default Player;
