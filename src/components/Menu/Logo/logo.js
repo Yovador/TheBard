0b4131ffbc2a700b2830a387eb7abe0db3d1d111
Yovador <yovador@gmail.com>
2020-11-10 17:14:03 +0100

    refactoring logo

--- src/components/Menu/Logo/logo.js
+++ src/components/Menu/Logo/logo.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import MenuButton from 'components/MenuButton/MenuButton';
-import styles from './logo.module.css'
-
+import styles from './logo.module.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 const Logo = (props) =>{
     const {imgPath, children, viewId} = props
 
     return (
-        <MenuButton  viewId = {viewId}>
+        <Link to={"/"+viewId}>
             <div className = {styles.logotest}>
-                <img src = {imgPath} alt="[Logo Not Loading]"/> <div className={styles.theBard}>{children}</div>
+                <img src = {imgPath} alt="[Logo Not Loading]"/>
+                <div className={styles.theBard}>{children}</div>
             </div>
-        </MenuButton>
+        </Link>
       );
 }
 
