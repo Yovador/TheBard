3bc02a62bb36a14b25bbc5d44aa8043479ccea66
AndrezRemi <andrez.remi@gmail.com>
2020-11-10 14:57:31 +0100

    compnent game and display

---
+++ src/Game/GameImg/DiplayGame.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DisplayGame = () =>{
+
+    return (
+        <div>
+         <p>
+             Image du jeu
+
+         </p>
+        </div>
+        
+      );
+
+}
+
+export default DisplayGame;
---
+++ src/Game/GameName/GameName.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const GameName = () =>{
+
+    return (
+        <div>
+         <p>
+             Nom du jeu et année
+
+         </p>
+        </div>
+        
+      );
+
+}
+
+export default GameName;
