1922f01edcf959302c782e33732b58264af71164
Yovador <yovador@gmail.com>
2020-11-12 15:21:34 +0100

    fix merge 2

--- src/components/Player/TrackButtons/PlayButton/Play.js
+++ src/components/Player/TrackButtons/PlayButton/Play.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-//import React, { useState } from 'react';
-=======
 
 import React, { useState } from 'react';
->>>>>>> 9158c1f7e3b3922e5b2a5b43b06133248c20d552
 import styles from "./Play.module.css";
 
 const Play = () =>{
 
-<<<<<<< HEAD
-    /*const [isPlaying, setIsPlaying] = useState(false);
-    const isPlaying = () => {
-=======
     const [isPlaying, setIsPlaying] = useState(false);
     const togglePlay = () => {
->>>>>>> 9158c1f7e3b3922e5b2a5b43b06133248c20d552
         setIsPlaying(!isPlaying)
         // if (blockIsVisible === true) {
         //   setBlockIsVisible(false)
@@ -24,7 +14,7 @@ const Play = () =>{
         //   setBlockIsVisible(true)
         // }
     }
-*/
+
     return (
         <div>
             <button onClick={togglePlay}>
