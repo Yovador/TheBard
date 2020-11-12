56ba1c11eae8cc04dc59d7ab3cbaca11ace3a4fc
Yovador <yovador@gmail.com>
2020-11-12 15:54:19 +0100

    Global component

---
+++ src/components/Global/Global.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Global.module.css';
+
+
+const Global = (props) =>{
+
+    const {children} = props
+
+    return (
+        <div>
+          {children}
+        </div>
+        
+      );
+
+}
+
+export default Global;
