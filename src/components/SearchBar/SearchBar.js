353c088b9c244581f36b80ad6ac461a7065821ad
FloMoro <flora.moreau@mmibordeaux.com>
2020-11-13 14:12:14 +0100

    putain persona est trop grand - yoann bouquey 14:12

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = (props) =>{
 
         <div className={styles.SearchBar}>
           <div className={styles.position}>
-            <input className={styles.input} type="text" id="name" name="name" requiredminlength="4" maxlength="8" size="20" placeholder="Recherche"/>
+            <input className={styles.input} type="text" id="name" name="name" requiredminlength="4" maxlength="10" size="20" placeholder="Recherche..."/>
             <img alt="" src="assets/image/loupe.svg"/>
            </div>
         </div>
