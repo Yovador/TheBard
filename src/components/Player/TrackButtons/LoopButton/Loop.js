f692f2b41393d80c4d29c09c9b495011c2bb8fcf
FloMoro <flora.moreau@mmibordeaux.com>
2020-11-13 14:58:40 +0100

    svg is OK

--- src/components/Player/TrackButtons/LoopButton/Loop.js
+++ src/components/Player/TrackButtons/LoopButton/Loop.js
@@ -21,14 +21,13 @@ const Loop = () =>{
 
                 {isLooping && (
                     <div className = {styles.TrackButton}>
-                        <img src = "assets/image/LoopHover.svg" alt = "Button not loaded" />
-                    </div>
+<svg xmlns="http://www.w3.org/2000/svg" width="18.428" height="25.339" viewBox="0 0 18.428 25.339"><g transform="translate(4524 10141.67)"><g transform="translate(-4524 -10141.67)"><path d="M73.214,155.5a6.912,6.912,0,0,1-6.11-10.141l-1.682-1.682a9.209,9.209,0,0,0,7.791,14.126v3.455l4.607-4.607-4.607-4.607Z" transform="translate(-64 -135.922)" fill="#ee2b47"/><path d="M153.941,3.455V0l-4.607,4.607,4.607,4.607V5.759a6.912,6.912,0,0,1,6.11,10.141l1.682,1.682a9.209,9.209,0,0,0-7.791-14.126Z" transform="translate(-144.727)" fill="#ee2b47"/></g></g></svg>                    </div>
                 )}
 
 
                 {!isLooping && (
                     <div className = {styles.TrackButton}>
-                        <img src = "assets/image/Loop.svg" alt = "Button not loaded" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18.428" height="25.339" viewBox="0 0 18.428 25.339"><g transform="translate(4524 10141.67)"><g transform="translate(-4524 -10141.67)"><path d="M73.214,155.5a6.912,6.912,0,0,1-6.11-10.141l-1.682-1.682a9.209,9.209,0,0,0,7.791,14.126v3.455l4.607-4.607-4.607-4.607Z" transform="translate(-64 -135.922)"/><path d="M153.941,3.455V0l-4.607,4.607,4.607,4.607V5.759a6.912,6.912,0,0,1,6.11,10.141l1.682,1.682a9.209,9.209,0,0,0-7.791-14.126Z" transform="translate(-144.727)" /></g></g></svg>
                     </div>
                 )}
 
